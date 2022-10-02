04f9e59097338ef02b3e66e0f4b3663645ab7f1c
D-Elbel <78599458+D-Elbel@users.noreply.github.com>
2022-10-02 16:12:57 +0100

    Login Page

--- app_server/controllers/forms.js
+++ app_server/controllers/forms.js
@@ -1,13 +1,13 @@
 /* GET home page */
-const registration = function (req, res) {
-  res.render("index", { title: "Registration" });
+const register = function (req, res) {
+  res.render("register", { title: "Registration" });
 };
 
 const login = function (req, res) {
-  res.render("login", { title: "Login" });
+  res.render("login", { title: "Login"});
 };
 
 module.exports = {
-  registration,
+  register,
   login
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const ctrlForms = require("../controllers/forms");
 const ctrlData = require("../controllers/data");
 
-router.get("/", ctrlForms.registration);
+router.get("/", ctrlForms.register);
 router.get("/login", ctrlForms.login);
 router.get("/workouts", ctrlData.workouts);
 
