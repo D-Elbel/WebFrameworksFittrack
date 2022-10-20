11f64fa9b086b886eed4011640b12f9a906723b7
D-Elbel <78599458+D-Elbel@users.noreply.github.com>
2022-10-20 22:58:03 +0100

    Created API

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
+const apiRoutes = require('./app_api/routes/index');
 
 
 var app = express();
@@ -20,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
