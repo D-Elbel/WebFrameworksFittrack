7dd38322110b6de7331ee147b3311060a0282e55
D-Elbel <78599458+D-Elbel@users.noreply.github.com>
2022-10-20 22:47:34 +0100

    Moved data to controller

--- app_server/controllers/data.js
+++ app_server/controllers/data.js
@@ -1,8 +1,29 @@
 /* GET home page */
 const workouts = function (req, res) {
-  res.render("workouts", { title: "Workouts" });
+  res.render("workouts", {
+    workouts:[ 
+      
+        {title: "Back & Biceps",
+        tags: ["Biceps", "Brachialis", "Traps", "Lats"],
+        exercises:[
+          {name: "Hammer Curls",sets: 3,reps: 3,resistance: 17.5,rest: 90,},
+          { name: "Bench Rows", sets: 3, reps: 8, resistance: 30, rest: 120 },
+          { name: "Shrugs", sets: 3, reps: 25, resistance: 32.5, rest: 120 },
+          { name: "Curls", sets: 3, reps: 12, resistance: 15, rest: 90 },
+        ]},
+
+        {title: "Chest & Triceps",
+        tags: ["Triceps", "Lateral Deltoid", "Rear Deltoid"],
+        exercises:[
+          {name: "Dumbbel Press",sets: 3,reps: 3,resistance: 25,rest: 120},
+          { name: "Front Riases", sets: 3, reps: 8, resistance: 17.5, rest: 90 },
+          { name: "Lateral Raises", sets: 3, reps: 12, resistance: 10, rest: 90 }          
+        ]}
+      
+    ]
+  });
 };
 
 module.exports = {
- workouts
+  workouts,
 };
